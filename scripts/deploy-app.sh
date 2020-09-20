774f266f967d1a4e02ae8f64f3e2f27e67c24da0
Keun Lee <keun.lee@thekeunster.com>
2020-09-20 16:51:51 -0500

    update jenkinsfile

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,5 +1,5 @@
 oc new-app --template=eap73-basic-s2i \
- -p IMAGE_STREAM_NAMESPACE=eap-demo \
+ -p IMAGE_STREAM_NAMESPACE=jboss-eap-test-001 \
  -p SOURCE_REPOSITORY_URL=https://github.com/keunlee/eap-test-app.git \
  -p SOURCE_REPOSITORY_REF=master \
  -p CONTEXT_DIR=helloworld-html5
